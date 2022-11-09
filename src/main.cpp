729587462b89112a3c7a59d1010f19546d00474e
Frederick Stein <fredericks@t-online.de>
2022-11-09 11:28:27 +0100

    Reintroduct check for validities of created Sudokus

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,13 @@
 #include <iostream>
 #include "Sudoku.hpp"
 
+using namespace std ;
+
 int main()
 {
   Sudoku sudoku1 ;
   sudoku1.print() ;
+  cout << sudoku1.is_valid() << endl ;
 
   constexpr size_t input_solved[] = {5, 3, 4, 6, 7, 8, 9, 1, 2,
                   6, 7, 2, 1, 9, 5, 3, 4, 8,
@@ -17,6 +20,7 @@ int main()
                   3, 4, 5, 2, 8, 6, 1, 7, 9} ;
   Sudoku sudoku2(input_solved) ;
   sudoku2.print() ;
+  cout << sudoku2.is_valid() << endl ;
 
   constexpr size_t input_error[] = {5, 3, 4, 6, 7, 8, 9, 1, 2,
                   6, 7, 2, 1, 9, 5, 3, 4, 8,
@@ -29,6 +33,7 @@ int main()
                   3, 4, 5, 2, 8, 6, 1, 7, 10} ;
   Sudoku sudoku3(input_error) ;
   sudoku3.print() ;
+  cout << sudoku3.is_valid() << endl ;
 
   return 0 ;
 }
