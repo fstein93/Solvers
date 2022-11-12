8c82de0887a3ed3fea7549d017fbc254f63e659c
Frederick Stein <fredericks@t-online.de>
2022-11-12 16:18:50 +0100

    Refactor utils.cpp

--- src/utils.cpp
+++ src/utils.cpp
@@ -2,14 +2,16 @@
 #include <iostream>
 #include "utils.hpp"
 
+using namespace std ;
+
 void utils::add_separator(const size_t i, const size_t line_length)
 {
   if ((i+1)%line_length)
   {
-    std::cout << " " ;
+    cout << " " ;
   }
   else
   {
-    std::cout << std::endl ;
+    cout << endl ;
   }
 }
